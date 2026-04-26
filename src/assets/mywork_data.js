db122d5d3ff52b1225b3e0b22113094b19aa5a3e
adnan saeed <121759955+malikadnan23@users.noreply.github.com>
2026-04-26 10:37:53 +0500

    updated work

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,5 +1,6 @@
 import project1_img from "../assets/project_1.jpg";
 import project2_img from "../assets/project_2.jpg";
+import project3_img from "../assets/project_3.jpg";
 
 const mywork_data = [
   {
@@ -11,6 +12,11 @@ const mywork_data = [
     w_no: 2,
     w_name: "Web design",
     w_img: project2_img,
+  },
+ {
+    w_no: 3,
+    w_name: "Web design",
+    w_img: project3_img,
   },
 ];
 
